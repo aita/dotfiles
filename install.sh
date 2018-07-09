aef2a5f22d17dc06b4df03836a63f235dc227a52
aita <792803+aita@users.noreply.github.com>
2018-07-10 08:39:20 +0900

    Move the nvim directory to the root

--- install.sh
+++ install.sh
@@ -4,7 +4,9 @@ ROOT_DIR=$(cd $(dirname $0) && pwd)
 UNAME=`uname`
 
 ln -sv $ROOT_DIR/.zshrc $HOME
-ln -sv $ROOT_DIR/.config $HOME
+
+mkdir -v $HOME/.config
+ln -sv $ROOT_DIR/nvim $HOME/.config/
 
 ln -sv $ROOT_DIR/.tmux.conf $HOME
 if [ "$UNAME" = "Darwin" ]; then
