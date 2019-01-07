9dbbbf42f37e8111f1116bb0b4d45269c97fd3b4
aita <792803+aita@users.noreply.github.com>
2019-01-07 15:24:55 +0900

    fix

---
+++ .emacs.d/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (undo-tree smartrep smartparens rtags projectile multiple-cursors flycheck-irony expand-region exec-path-from-shell counsel company-irony aggressive-indent))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- .emacs.d/install.el
+++ .emacs.d/install.el
@@ -1,29 +1,13 @@
-;; MELPAをパッケージレポジトリに追加
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(package-initialize)
-(unless package-archive-contents (package-refresh-contents))
+(defvar custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file)
 
+  ;; MELPAをパッケージレポジトリに追加
+  (require 'package)
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+  (package-initialize)
+  (unless package-archive-contents (package-refresh-contents))
 
-(defvar install-packages
-      '(smartparens
-        swiper
-        undo-tree
-        expand-region
-        smartrep
-        counsel
-        projectile
-        multiple-cursors
-        exec-path-from-shell
-        company
-        irony
-        rtags
-        flycheck
-        flycheck-irony
-        company-irony
-        aggressive-indent))
-
-
-(dolist (package install-packages)
-  (unless (package-installed-p package)
-    (package-install package)))
+  (dolist (package package-selected-packages)
+    (unless (package-installed-p package)
+    (package-install package))))
