8f3146ec829525e6c5a83b156d58ae177b8f298c
aita <792803+aita@users.noreply.github.com>
2019-01-08 10:30:18 -0800

    install cmake-mode

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (company-irony-c-headers clang-format diminish magit srefactor quickrun editorconfig bison-mode fish-mode undo-tree smartrep smartparens rtags projectile multiple-cursors flycheck-irony expand-region exec-path-from-shell counsel company-irony aggressive-indent)))
+    (cmake-mode company-irony-c-headers clang-format diminish magit srefactor quickrun editorconfig bison-mode fish-mode undo-tree smartrep smartparens rtags projectile multiple-cursors flycheck-irony expand-region exec-path-from-shell counsel company-irony aggressive-indent)))
  '(semantic-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
