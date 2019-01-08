d095dc76dfde490442e86061a65f83d303ea8731
aita <792803+aita@users.noreply.github.com>
2019-01-08 12:58:07 +0900

    install magit

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -5,7 +5,8 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (srefactor quickrun editorconfig bison-mode fish-mode undo-tree smartrep smartparens rtags projectile multiple-cursors flycheck-irony expand-region exec-path-from-shell counsel company-irony aggressive-indent))))
+    (magit srefactor quickrun editorconfig bison-mode fish-mode undo-tree smartrep smartparens rtags projectile multiple-cursors flycheck-irony expand-region exec-path-from-shell counsel company-irony aggressive-indent)))
+ '(semantic-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
