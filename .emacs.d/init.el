fa7bbff38a9de794df6c9e786b9fafa203066b18
aita <792803+aita@users.noreply.github.com>
2019-01-14 04:35:30 +0900

    OPAM

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -239,9 +239,26 @@
 
 
 ;; OCaml
-(add-hook 'tuareg-mode-hook 'merlin-mode)
+(when (file-exists-p (expand-file-name ".opam" user-emacs-directory))
+  ;; -- opam and utop setup --------------------------------
+  ;; Setup environment variables using opam
+  (dolist
+      (var (car (read-from-string
+                 (shell-command-to-string "opam config env --sexp"))))
+    (setenv (car var) (cadr var)))
+  ;; Update the emacs path
+  (setq exec-path (split-string (getenv "PATH") path-separator))
+  ;; Update the emacs load path
+  (push (concat (getenv "OCAML_TOPLEVEL_PATH")
+                "/../../share/emacs/site-lisp") load-path)
+  ;; Automatically load utop.el
+  (autoload 'utop "utop" "Toplevel for OCaml" t)
+  (autoload 'utop-setup-ocaml-buffer "utop" "Toplevel for OCaml" t)
+  (add-hook 'tuareg-mode-hook 'utop-setup-ocaml-buffer)
 
-(require 'ocp-indent)
-(add-hook 'tuareg-mode-hook
-     '(lambda ()
-        (add-hook 'local-write-file-hooks 'ocp-indent-buffer)))
+
+  (add-hook 'tuareg-mode-hook 'merlin-mode)
+  (require 'ocp-indent)
+  (add-hook 'tuareg-mode-hook
+            '(lambda ()
+               (add-hook 'local-write-file-hooks 'ocp-indent-buffer))))
